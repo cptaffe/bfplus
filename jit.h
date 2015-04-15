8a8529832b6c5ad48d5961ae884863d33cf61a1a
Connor Taffe <cpaynetaffe@gmail.com>
2015-04-14 19:16:32 -0500

    added archetecture specific operations in a generic way

--- jit.h
+++ jit.h
@@ -6,6 +6,36 @@
 
 namespace bf {
 
+// interface for architecture
+class architecture {
+public:
+	// listing of operations
+	enum operation {
+		MOV_64,
+		MOV_CX_SI,
+		MOV_DX_64,
+		MOV_DI_64,
+		MOV_AX_64,
+		ADD_LIT8,
+		ADD_LIT8_LOCK,
+		ADD_LIT8_SI,
+		ADD_LIT8_SI_LOCK,
+		LOOP,
+		SYSCALL,
+		RET,
+		MOV_SI_AX,
+		MOV_AS_LIT64
+	};
+
+	virtual ~architecture() {}
+	virtual const uint8_t *get(enum operation op) const = 0;
+};
+
+class arch_x86 : public architecture {
+public:
+	virtual const uint8_t *get(enum operation op) const;
+};
+
 class jit {
 public:
 	jit(size_t mem_pages = 1, size_t exec_pages = 1);
