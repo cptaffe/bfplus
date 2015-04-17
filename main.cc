5e558263da9617b95848d1d7ff77c00c3b524200
Connor Taffe <cpaynetaffe@gmail.com>
2015-04-16 20:21:11 -0500

    moved test code to test class, main just declares a test object

--- main.cc
+++ main.cc
@@ -3,9 +3,24 @@
 
 #include <sstream>
 
-int main() {
-	std::istringstream s("this is a string");
-	bf::comp c(&s);
+class test {
+public:
+	test();
+	~test();
+private:
+	// initialized in order.
+	std::istream *in;
+	bf::comp c;
+};
 
+test::test() :
+	in(static_cast<std::istream *>(new std::istringstream("this is a string"))),
+	c(in) {
 	c.run();
 }
+
+test::~test() {
+	delete in;
+}
+
+int main() { test t; }
